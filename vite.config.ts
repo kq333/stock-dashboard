926ad57af7157e79d253e0ae14b2d8f87b5a0e5f
a.fugowski <a.fugowski@mymusic.pl>
2026-07-23 00:22:34 +0200

    add flag envPrefix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  envPrefix: ['VITE_', 'NEWS_'],
   plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
